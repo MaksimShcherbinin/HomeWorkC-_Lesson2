0a33fc86a941840bba67cc9b8a6bb9f363d722d7
Maksim <maksim6erbinin@mail.ru>
2022-06-29 03:16:23 +0300

    Added Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+while (userNumber > 99) userNumber = (userNumber - userNumber % 10) / 10;
+
+Console.WriteLine("Вторая цифра введенного числа: " + userNumber%10);
