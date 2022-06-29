85425242deea0d2ed3ddc28d42704a96114e33d6
Maksim <maksim6erbinin@mail.ru>
2022-06-29 03:25:16 +0300

    Added Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if(userNumber < 100) Console.WriteLine("Третьей цифры числа " + userNumber + " нет!");
+else
+{
+   while (userNumber > 999) userNumber = (userNumber - userNumber % 10) / 10;
+   Console.WriteLine("Третья цифра введенного числа: " + userNumber%10); 
+}
