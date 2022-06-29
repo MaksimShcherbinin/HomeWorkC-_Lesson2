ff8a1b9e48f04e2b86a2881ced7a3a048c0c0b0e
Maksim <maksim6erbinin@mail.ru>
2022-06-29 20:37:30 +0300

    Added snail task

---
+++ SnailTask/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите высоту шеста (м): ");
+int pole_height = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите высоту подъема улитки за день по шесту (м): ");
+int way_up = Convert.ToInt32(Console.ReadLine());
+int way_down, day = 0;
+do
+{
+    Console.WriteLine("Введите высоту спуска улитки за ночь по шесту из условия, что высота подъема должна быть больше (м): ");
+    way_down = Convert.ToInt32(Console.ReadLine());
+}
+while (way_up <= way_down);
+while (pole_height > (day * (way_up - way_down) + way_up)) day++;
+Console.WriteLine("Улитка доползет до вершины шеста на " + (day + 1) + " день.");
