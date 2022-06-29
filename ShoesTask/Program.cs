5f10b46f62d125f2550cccfc9e79aae5b184ca10
Maksim <maksim6erbinin@mail.ru>
2022-06-29 22:01:43 +0300

    Added shoes task

---
+++ ShoesTask/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите расстояние между рядами (мм): "); //a
+int row_spacing = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите расстояние между дырочками в ряду (мм): "); //b
+int distance_holes = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину свободного конца шнурка (мм): "); //l
+int free_end_length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество дырочек в каждом ряду (шт): "); //N
+int number_of_holes = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Длинна шнурка составит " + (row_spacing + (number_of_holes - 1) * 2 * (distance_holes + row_spacing) + 2 * free_end_length) + " мм.");
