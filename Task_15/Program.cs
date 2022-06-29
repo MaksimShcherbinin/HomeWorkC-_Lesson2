183cba93f0bf607930e8acf6dc5669521d40ef71
Maksim <maksim6erbinin@mail.ru>
2022-06-29 03:38:16 +0300

    Added Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if((userNumber%6 == 0) || (userNumber%7 == 0)) Console.WriteLine(userNumber + "-й день недели выходной!");
+else Console.WriteLine(userNumber + "-й день недели НЕ выходной!");
